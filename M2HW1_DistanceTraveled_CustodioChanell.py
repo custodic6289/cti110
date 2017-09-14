2f26bb7ba3f2f04f5667233d4607d0f21c34d1c2
custodic6289 <31895594+custodic6289@users.noreply.github.com>
2017-09-14 12:41:27 -0400

    Add files via upload

---
+++ M2HW1_DistanceTraveled_CustodioChanell.py
@@ -0,0 +1,25 @@
+# CTI-110
+# M2HW1 - Distance Traveled
+# Chanell Custodio
+# 9/14/17
+# Calculate and display the distance traveled
+
+
+# Constant Variable: SPEED = 70
+SPEED = 70
+
+# Variable: distanceAFter6Hours = SPEED*6
+distanceAfter6Hours = SPEED*6
+
+# Variable: distanceAfter10Hours = SPEED*10
+distanceAfter10Hours = SPEED*10
+
+# Variable: distanceAfter15Hours = SPEED*15
+distanceAfter15Hours = SPEED*15
+
+
+print('The car will travel the following distances: ')
+print(distanceAfter6Hours, 'miles in 6 hours.')
+print(distanceAfter10Hours, 'miles in 10 hours.')
+print(distanceAfter15Hours, 'miles in 15 hours.')
+

2b54282764fbb8476394fe9b278fa5245531a739
custodic6289 <31895594+custodic6289@users.noreply.github.com>
2017-09-26 11:07:04 -0400

    Add files via upload

---
+++ PopQuiz_01_CustodioChanell.py
@@ -0,0 +1,20 @@
+# CTI 110
+# Pop Quiz 01
+# Chanell Custodio
+# 9/26/17
+# Finds the average of two numbers
+
+
+
+# Get the first number from the user
+first_number = float(input('Enter the first number: '))
+
+# Get the second number from the user
+second_number = float(input('Enter the second number: '))
+
+# Perform calculation to find average
+calculation = first_number + second_number
+average = calculation / 2                  
+
+# Display the average of the 2 numbers
+print('The Average is: ', average)

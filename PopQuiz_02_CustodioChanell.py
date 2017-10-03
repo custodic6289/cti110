8eda705a556121e698d02bdd4fd20001f2366a76
custodic6289 <31895594+custodic6289@users.noreply.github.com>
2017-10-03 12:15:17 -0400

    Add files via upload

---
+++ PopQuiz_02_CustodioChanell.py
@@ -0,0 +1,24 @@
+# CTI 110
+# Pop Quiz 2
+# Chanell Custodio
+# 10/3/17
+# Display a message using an if statement depending on the temperature
+
+
+
+def main():
+    
+    # Identify the  tempurature
+    temperature = float(input('Enter the temperature: '))
+
+
+    if temperature >= 40:
+        print("Nice weather we're having.")
+    else:
+        print("A little cold, isn't it?")
+
+# Program start and run 3 times
+main()
+main()
+main()
+    
